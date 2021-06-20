eb4243e4b4ee620f767fe288f02af5e6afe1a0ee
Rohan Gupta <56235204+rohan9896@users.noreply.github.com>
2021-06-20 20:31:49 +0530

    feat: form component completed

--- src/components/FormComponent/FormComponent.js
+++ src/components/FormComponent/FormComponent.js
@@ -1,40 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import "./FormComponent.css";
 import {
   Box,
   Text,
   FormControl,
-  FormLabel,
   Input,
-  Flex,
   Button,
 } from "@chakra-ui/react";
+import axios from "axios";
 
 function FormComponent() {
+
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [userId, setUserId] = useState(0);
+
+  const [loading, setLoading] = useState(false);
+  
+  const [success, setSuccess]= useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    
+    const response = await axios.post('https://jsonplaceholder.typicode.com/posts', {
+      title,
+      body,
+      userId
+    })
+
+    setLoading(false);
+    
+    if(response.data) {
+      setSuccess("Submitted successfully!");
+      setTimeout(() => setSuccess(''), 2000)
+    }
+  }
+
   return (
     <Box className="FormComponent">
-      <Text>JsonPlaceHolder POST</Text>
-      <FormControl>
-        <Box className="FormComponent__InputsContainer">
-          <Input type="text" placeholder="Title" required />
-          <Input type="text" placeholder="Body" required />
-          <Input
-            type="Number"
-            placeholder="UserId"
-            required
-            errorBorderColor="red"
-          />
-        </Box>
-        <Button
-          width="7rem"
-          variant="solid"
-          isLoading={false}
-          loadingText="Submitting"
-          style={{ color: "#F9FAFB", background: "#1F2937" }}
-        >
-          Submit
-        </Button>
-      </FormControl>
+      <Box className="FormComponent__Heading">
+        <Text className="FormComponent__Heading__Text">
+          JsonPlaceHolder POST
+        </Text>
+      </Box>
+      <form onSubmit={handleSubmit}>
+        <FormControl>
+          <Box className="FormComponent__InputsContainer">
+            <Input type="text" placeholder="Title" required />
+            <Input type="text" placeholder="Body" required />
+            <Input
+              type="Number"
+              placeholder="UserId"
+              required
+              errorBorderColor="red"
+            />
+          </Box>
+          {success && <p className="successMsg">{success}</p>}
+          <Button
+            type="submit"
+            width="7rem"
+            variant="solid"
+            isLoading={loading}
+            loadingText="Submitting"
+            style={{ color: "#F9FAFB", background: "#1F2937", padding: "0.5rem" }}
+          >
+            Submit
+          </Button>
+        </FormControl>
+      </form>
     </Box>
   );
 }
